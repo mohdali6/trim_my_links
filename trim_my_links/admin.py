9c939a920d66d7fe4403d16baa2c46c0d4211c3b
mali <mohd.ali6@yahoo.com>
2018-09-19 21:15:46 +0530

    Admin site changes done

--- trim_my_links/admin.py
+++ trim_my_links/admin.py
@@ -3,4 +3,16 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Link
 # Register your models here.
+
+
+class LinkAdmin(admin.ModelAdmin):
+	list_display = [field.name for field in Link._meta.get_fields()]
+
+	fieldsets = [
+		('New Generated Trimmed Link', {'fields': ['trimmed_link']}),
+		('Original Big Link', {'fields': ['original_link']})
+	]
+
+admin.site.register(Link, LinkAdmin)
--- trim_my_links/models.py
+++ trim_my_links/models.py
@@ -4,3 +4,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Link(models.Model):
+	trimmed_link = models.CharField(max_length=8, unique=True)
+	original_link = models.TextField(unique=True)
+	date_trimmed = models.DateTimeField('date trimmed', auto_now_add=True)
+
+	def __str__(self):
+		return self.trimmed_link
